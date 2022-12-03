416668d878a9a12745bb96b1e5ec3f69e9ef26d3
Mohamed Amine BELKAIED <m.belkaied@openbee.com>
2022-12-03 18:32:32 +0100

    update CM

--- clothing-management-backend/src/main/java/com/clothing/management/repository/IFbPageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clothing.management.repository;
-
-import com.clothing.management.entities.FbPage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IFbPageRepository extends JpaRepository<FbPage, Long > {
-}
--- clothing-management-backend/src/main/java/com/clothing/management/repository/IProductsPacketRepository.java
+++
@@ -1,8 +0,0 @@
-package com.clothing.management.repository;
-
-import com.clothing.management.entities.ProductsPacket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProductsPacketRepository extends JpaRepository<ProductsPacket , Long> {
-
-}
